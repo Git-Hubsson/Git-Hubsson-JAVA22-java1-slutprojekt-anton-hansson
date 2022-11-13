e7e29d45fd5268a3818ec8369bc6d762179e306f
Git-Hubsson <112413627+Git-Hubsson@users.noreply.github.com>
2022-11-13 11:43:11 +0100

    Add files via upload

--- GetDate.java
+++ GetDate.java
@@ -6,9 +6,8 @@ import java.util.Locale;
 public class GetDate {
     String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
     LocalDate now = LocalDate.now();
-
+    TemporalField fieldISO = WeekFields.of(Locale.UK).dayOfWeek();
     public LocalDate thisWeeksDates(int x) {
-        TemporalField fieldISO = WeekFields.of(Locale.UK).dayOfWeek();
         return now.with(fieldISO, x);
     }
 
